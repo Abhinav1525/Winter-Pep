67369974c2774da092b49412e536ffde2d436bd7
Abhinav Bansal <118711459+Abhinav1525@users.noreply.github.com>
2025-02-03 23:30:47 +0530

    Create App.js

---
+++ Day6/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+// import Header from './component/Header';
+// import Footer from './component/Footer';
+import Parent from './component/Parent';
+// import SportsClick from './component/SportsClick';
+// import FixedDeposit from './component/FixedDeposit';
+
+function App() {
+  return (
+    <div>
+      {/* <Header />
+      <Footer /> */}
+      <Parent/>
+      {/* <SportsClick/>
+      <FixedDeposit/> */}
+    </div>
+  );
+}
+
+export default App;
