a05a8d3db25861abc8953e5b58d87a2e4a94930a
Abhinav Bansal <118711459+Abhinav1525@users.noreply.github.com>
2025-02-03 23:48:30 +0530

    Create App.js

---
+++ Day11/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+// import {Component1} from './Testing';
+import {Home} from './ECommerce';
+
+function App() {
+  return (
+    <div className='App'>
+      {/* <Component1 /> */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
