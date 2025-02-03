49558ff205900953607d7f45dd813a0cda4b4305
Abhinav Bansal <118711459+Abhinav1525@users.noreply.github.com>
2025-02-03 23:52:13 +0530

    Create Utility.js

---
+++ Day12/components/Utility.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Utility = () => {
+    return (
+        <div>Utility</div>
+    );
+}
+
+export function HyperUtility() {
+    return (
+        <div>
+            <p>I am a helper function</p>
+        </div>
+    );
+}
+
+export function HyperUtility2() {
+    return (
+        <div>
+            <p>I am a helper function 2</p>
+        </div>
+    );
+}
+
+export default Utility;
