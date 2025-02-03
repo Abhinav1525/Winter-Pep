74ba803c1d1c407a5fe567825fd2beabb98f5ee6
Abhinav Bansal <118711459+Abhinav1525@users.noreply.github.com>
2025-02-03 23:43:17 +0530

    Create SportsClick.js

---
+++ Day9/SportsClick.js
@@ -0,0 +1,11 @@
+function sports() {
+    const call = () => {
+        alert("I like playing football");
+    }
+    return (
+        <div>
+            <button onClick={call}>Click Here</button>
+        </div>
+    );
+}
+export default sports;
