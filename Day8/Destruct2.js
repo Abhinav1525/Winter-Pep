59e080cc8ea22c9014620c30765f0383d6ae5b85
Abhinav Bansal <118711459+Abhinav1525@users.noreply.github.com>
2025-02-03 23:41:29 +0530

    Create Destruct2.js

---
+++ Day8/Destruct2.js
@@ -0,0 +1,43 @@
+// CALLBACK FUNCTION
+
+// function sample(val) {
+//     console.log(val(),"I am a calling function");
+// }
+// function test() {
+//     console.log("I am a called function");
+//     return "called, ";
+// }
+// sample(test);
+
+
+// function BookNo(callback) {
+//     console.log("This is BookNo");
+//     callback();
+// }
+// function BookName(callback) {
+//     console.log("This is BookName");
+//     callback();
+// }
+// function Author() {
+//     console.log("This is Author");
+// }
+// BookNo(() => BookName(Author));
+
+
+
+import {useState} from "react";
+import {test} from 'Sample';
+export function Maintain() {
+    return (
+        <>
+
+        </>
+    );
+}
+export function test() {
+    return (
+        <>
+
+        </>
+    );
+}
