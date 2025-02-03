66e87a54393a0553c4e61ffc534836e3bfb98453
Abhinav Bansal <118711459+Abhinav1525@users.noreply.github.com>
2025-02-03 23:41:54 +0530

    Create Destruct3.js

---
+++ Day8/Destruct3.js
@@ -0,0 +1,19 @@
+// NAMED COMPONENTS AND USE OF DESTRUCTUIRNG
+
+import {useState} from "react";
+import {test} from 'Day8';
+export function Maintain() {
+    const {name, setName} = useState("");
+    return (
+        <>
+            <p>(name)</p>
+        </>
+    );
+}
+export function test() {
+    return (
+        <>
+
+        </>
+    );
+}
