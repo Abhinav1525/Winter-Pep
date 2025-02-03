e21345db7e448ce0dd81b1fca7eea1fc1340ad2c
Abhinav Bansal <118711459+Abhinav1525@users.noreply.github.com>
2025-02-03 23:39:00 +0530

    Create Destruct.js

---
+++ Day7/Destruct.js
@@ -0,0 +1,19 @@
+// const user = {
+//     regNo:100,
+//     uname: "lpu",
+//     age: 20
+// }
+
+// let {uname, age} = user;
+// console.log(unamename);
+// console.log(age);
+
+
+function Child(props) {
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+}
+export default Child;
