fb02c32e079ee16547a1abdd59b79a839a2f7ae7
Abhinav Bansal <118711459+Abhinav1525@users.noreply.github.com>
2025-02-03 23:44:13 +0530

    Create App.js

---
+++ Day10/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Visuals } from './components/Visuals';
+
+function App() {
+  return (
+    <div>
+      <Visuals  />
+    </div>
+  );
+}
+
+export default App;
