a67d69be7733cfea4f948b01c965e675544ae298
Abhinav Bansal <118711459+Abhinav1525@users.noreply.github.com>
2025-02-03 23:29:30 +0530

    Add files via upload

---
+++ Day6/components/Child.js
@@ -0,0 +1,19 @@
+// function Child(props) {
+//   return (
+//     <div>
+//       <p>Hi!, How are you? {props.myName}</p>
+//     </div>
+//   );
+// }
+// export default Child;
+
+
+
+function Child(props) {
+  return (
+    <div>
+      <p>ReactJs Book has been written by {props.AuthorName}</p>
+    </div>
+  );
+}
+export default Child;
