b84911b4724ba131b18cf7fc329cbdf7d55c4288
Abhinav Bansal <118711459+Abhinav1525@users.noreply.github.com>
2025-02-03 23:29:04 +0530

    Create Parent.js

---
+++ Day6/components/Parent.js
@@ -0,0 +1,20 @@
+import Child from "./Child"
+// function Parent() {
+//     return(
+//         <div>
+//             <Child myName = "Morning Batch"/>
+//         </div>
+//     )
+// }
+// export default Parent;
+
+
+
+function Parent() {
+    return(
+        <div>
+            <Child AuthorName = "XYZ"/>
+        </div>
+    )
+}
+export default Parent;
